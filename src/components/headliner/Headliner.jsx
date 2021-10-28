ec503c64235c12dd6b9ae635ae9cc492cb0ef6f5
andreisokhan <sokhanpageproofs@gmail.com>
2021-10-28 22:33:28 +0300

    2021/10/28_22-33_react-project

---
+++ src/components/headliner/Headliner.jsx
@@ -0,0 +1,36 @@
+import './Headliner.css'
+import thumbnaill from "./../...."
+import clockIcon from "./../...."
+import likeIkon from "./../...."
+import dislikeIcon from "./../...."
+
+
+function Headliner() {
+   return (
+      <article className="headliner">
+         <img src="" alt="" />
+         <div>
+            <div>
+               <div>
+                  <div>
+            <img src="" alt="" />
+            <span>10.8 k</span>
+            </div>
+            <div>
+            <img src="" alt="" />
+            <span>1.4 k</span>
+            </div>
+         </div>
+         <div>
+            <span>BBC NEWS</span>
+         <div>
+         <img src="" alt="" />
+         <span>2:00 pm</span>
+         </div>
+         </div>
+         </div>
+         </article>
+   )
+}
+
+export default Headliner
