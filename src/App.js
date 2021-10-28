a37a5da4a0fb3f86ae478c838b6d6cdab8c61c11
andreisokhan <sokhanpageproofs@gmail.com>
2021-10-29 00:58:11 +0300

    2021/10/29_01-00_reakt-project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import logo from './logo.svg';
-import './App.css';
+
 import Headliner from './components/headliner/Headliner';
 
 function App() {
--- src/components/headliner/Headliner.jsx
+++ src/components/headliner/Headliner.jsx
@@ -1,36 +1,36 @@
-import './Headliner.css'
-import thumbnaill from "./../...."
-import clockIcon from "./../...."
-import likeIkon from "./../...."
-import dislikeIcon from "./../...."
+import './Headliner.css';
+import thumbnail from "./../../assets/img/image-1.jpg";
+import likeIkon from "./../../assets/img/like.svg";
+import dislikeIcon from "./../../assets/img/dislike.svg";
+import clockIcon from "./../../assets/img/clock.svg"
 
 
 function Headliner() {
    return (
       <article className="headliner">
-         <img src="" alt="" />
-         <div>
-            <div>
-               <div>
-                  <div>
-            <img src="" alt="" />
-            <span>10.8 k</span>
+         <img className="thumbnail-img" src={thumbnail} alt="thumbnail-icon" />
+         <div className="info">
+            <h1 className="title">Hate speech vs free speech</h1>
+            <div className="rating">
+               <div className="like">
+                  <img src={likeIkon} alt="like-icon" />
+                  <span className="text">10.8k</span>
+               </div>
+               <div className="dislike">
+                  <img src={dislikeIcon} alt="dislike-icon" />
+                  <span className="text">1.4k</span>
+               </div>
             </div>
-            <div>
-            <img src="" alt="" />
-            <span>1.4 k</span>
+            <div className="news">
+               <span className="label">BBC NEWS</span>
+               <div className="time">
+                  <img className="time-icon" src={clockIcon} alt="clock-icon" />
+                  <span className="time-text">2:00 pm</span>
+               </div>
             </div>
          </div>
-         <div>
-            <span>BBC NEWS</span>
-         <div>
-         <img src="" alt="" />
-         <span>2:00 pm</span>
-         </div>
-         </div>
-         </div>
-         </article>
-   )
+      </article>
+   );
 }
 
-export default Headliner
+export default Headliner;
